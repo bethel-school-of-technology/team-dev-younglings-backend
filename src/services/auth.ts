d350abcad55e510aa55626734aec332c37af0804
kal-dave <103179705+kal-dave@users.noreply.github.com>
2024-03-07 15:54:31 +0400

    created token assigner functionn

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { User } from '../models/user';
 
 export const hashedPassword = async (inputtedText: string) => {
     const saltRound = 12;
@@ -7,4 +9,16 @@ export const hashedPassword = async (inputtedText: string) => {
 
 export const comparePasswords = async (inputtedText: string, hashedPassword: string) => {
     return await bcrypt.compare(inputtedText, hashedPassword);
+}
+
+const secret = "We are secretly Jedi of the Old Republic!";
+
+export const tokenAssigner = async (user:User) => {
+    let token = jwt.sign(
+        {userId: user.userId},
+        secret,
+        {expiresIn: '2hr'}
+    )
+
+    return token;
 }
